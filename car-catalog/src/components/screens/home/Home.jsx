d57cfef370ab84e60a27fd199abe39df3160ff69
Wall <ilnazamirhanov61@gmail.com>
2023-05-16 19:31:22 +0300

    add image

--- car-catalog/src/components/screens/home/Home.jsx
+++ car-catalog/src/components/screens/home/Home.jsx
@@ -1,20 +1,22 @@
-import { useState } from 'react'
-import '../../../assets/styles/global.css'
-import styles from './Home.module.css'
+import React from 'react';
+import { useState } from 'react';
+import '../../../assets/styles/global.css';
+import styles from './Home.module.css';
+
 function Home() {
-  return (
+    return (
         <div>
             <h1>Cars catalog 1</h1>
-        <div>
-            <div className={styles.item}>
-                <h2>Car 1</h2>
-                <p>$100 000</p>
-                <button>Read more</button>
+            <div>
+                <div className={styles.item}>
+                    <div className={styles.image} style={{ backgroundImage: 'url(/2.png)' }}></div>
+                    <h2>Car 1</h2>
+                    <p>$100 000</p>
+                    <button>Read more</button>
+                </div>
             </div>
         </div>
-        </div>
-
-  )
+    );
 }
 
-export default Home
+export default Home;
