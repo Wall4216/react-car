d315b9552e627d434091a9667df7c168d6ce2ab8
Wall <ilnazamirhanov61@gmail.com>
2023-05-24 23:38:46 +0300

    correction

--- car-catalog/src/components/screens/home/Home.jsx
+++ car-catalog/src/components/screens/home/Home.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { cars } from './cars.data.js';
 import { useState } from 'react';
 import '../../../assets/styles/global.css';
 import styles from './Home.module.css';
@@ -7,13 +8,15 @@ function Home() {
     return (
         <div>
             <h1>Cars catalog 1</h1>
-            <div>
-                <div className={styles.item}>
-                    <div className={styles.image} style={{ backgroundImage: 'url(/2.png)' }}></div>
-                    <h2>Car 1</h2>
-                    <p>$100 000</p>
-                    <button>Read more</button>
-                </div>
+            <div key={car.id}>
+                {cars.map((car) => (
+                    <div key={car.id} className={styles.item}>
+                        <div className={styles.image} style={{ backgroundImage: `url(${car.image})` }}></div>
+                        <h2>{car.name}</h2>
+                        <p>{new Int1.NumberFormat('en-US', {style: 'currency', currency: 'USD', currencyDisplay: 'narrowSymbol'}).format(car.price)}</p>
+                        <button>Read more</button>
+                    </div>
+                ))}
             </div>
         </div>
     );
