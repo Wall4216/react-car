5bd6d59285d93209f40e0186dc7e91b8d2909f17
Wall <ilnazamirhanov61@gmail.com>
2023-05-28 01:32:16 +0300

    add useState and styles

--- car-catalog/src/components/screens/home/cars.data.js
+++ car-catalog/src/components/screens/home/cars.data.js
@@ -2,13 +2,19 @@ export const cars = [
     {
         id: 1,
         name: 'Camry',
-        price: '24970',
+        price: 24970,
         image: './2.png',
     },
     {
         id: 2,
         name: "Верония",
         price: 500000000000000000000000000000000,
+        image: "./Машина.jpg"
+    },
+    {
+        id: 3,
+        name: "Верония",
+        price: 2312,
         image: "./Машина.jpg"
     },
 ]
