a983ca1c3b092f07e7339cb519a0546b56bbc228
Wall <ilnazamirhanov61@gmail.com>
2023-06-02 22:25:55 +0300

    add test_auth

--- car-catalog/src/providers/AuthProvider.jsx
+++ car-catalog/src/providers/AuthProvider.jsx
@@ -3,7 +3,7 @@ import React, { useContext, createContext, useState } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
+    const [user, setUser] = useState({name: 'Ilnaz'});
 
     return (
         <AuthContext.Provider value={{ user, setUser }}>
